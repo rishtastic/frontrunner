31259771e556040a795e3e87568c0d4c31d21992
Rishabh Daga <dagarishabh@gmail.com>
2024-04-12 22:05:57 +0530

    adds panic recovery

--- runner/runner.go
+++ runner/runner.go
@@ -118,28 +118,3 @@ func (r *Runner[T]) FirstKWithTimeout(k int, d time.Duration) ([]T, bool, error)
 		return res, true, nil
 	}
 }
-
-func raceTasks[T any](tasks []func() T, k int, rch chan<- []T) {
-	ch := make(chan T, len(tasks))
-	for _, task := range tasks {
-		t := task
-		go taskChannel(ch, t)
-	}
-	res := []T{}
-	for range k {
-		r := <-ch
-		res = append(res, r)
-	}
-	rch <- res
-}
-
-func taskChannel[T any](ch chan<- T, t func() T) {
-	r := t()
-	ch <- r
-}
-
-func timeout(ch chan<- bool, d time.Duration) {
-	time.AfterFunc(d, func() {
-		ch <- true
-	})
-}
